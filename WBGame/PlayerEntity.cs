4506acd1439f96c390067baf337a00e8b568d378
anvemaha <anvemaha@gmail.com>
2020-06-11 19:52:06 +0300

    Try separate classes

---
+++ WBGame/PlayerEntity.cs
@@ -0,0 +1,56 @@
+﻿using Otter;
+
+namespace WBGame
+{
+    class PlayerEntity : Entity
+    {
+        public float MoveSpeed;
+        public const float MoveSpeedSlow = 5;
+        public const float MoveSpeedFast = 10;
+
+        public PlayerEntity(float x, float y) : base(x, y)
+        {
+            var image = Image.CreateRectangle(32);
+            AddGraphic(image);
+            image.CenterOrigin();
+            MoveSpeed = MoveSpeedSlow;
+        }
+
+        public void SetColor()
+        {
+            Graphic.Color = Color.Green;
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            if (Input.KeyPressed(Key.G))
+                SetColor();
+
+            if (Input.KeyDown(Key.W))
+                Y -= MoveSpeed;
+
+            if (Input.KeyDown(Key.S))
+                Y += MoveSpeed;
+
+            if (Input.KeyDown(Key.A))
+                X -= MoveSpeed;
+
+            if (Input.KeyDown(Key.D))
+                X += MoveSpeed;
+
+            if (Input.KeyPressed(Key.Space))
+                if (MoveSpeed == MoveSpeedSlow)
+                {
+                    MoveSpeed = MoveSpeedFast;
+                    Graphic.Color = Color.Red;
+                }
+                else
+                {
+                    MoveSpeed = MoveSpeedSlow;
+                    Graphic.Color = Color.White;
+                }
+        }
+    }
+}
---
+++ WBGame/Program.cs
@@ -0,0 +1,15 @@
+﻿using Otter;
+
+namespace WBGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new Game("Worm bricks", 1280, 720, 144);
+            Scene scene = new Scene();
+            scene.Add(new PlayerEntity(Game.Instance.HalfWidth, Game.Instance.HalfHeight));
+            game.Start(scene);
+        }
+    }
+}
