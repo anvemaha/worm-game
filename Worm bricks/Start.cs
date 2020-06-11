f74add46a0453fed4dec1c711cb6d776af452697
anvemaha <anvemaha@gmail.com>
2020-06-11 15:56:26 +0300

    Add project files and changelog

---
+++ Worm bricks/Start.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Otter;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Worm_bricks
+{
+    public class Start
+    {
+        /// <summary>
+        /// Setting up ComTest
+        /// </summary>
+        /// <param name="game"></param>
+        /// <example>
+        /// <pre name="test">
+        ///  1 === 1;
+        /// </pre>
+        /// </example>
+        public static void Begin(Game game)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
