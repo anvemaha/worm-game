396c31f3f2cd63c3afea1097d3df295cca78d6d4
anvemaha <anvemaha@gmail.com>
2020-06-20 08:55:41 +0300

    Prepare for a more major rework

---
+++ WBGame/Misc/Block.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WBGame.Pooling;
+using Otter;
+
+namespace WBGame.Misc
+{
+    class Block : Poolable
+    {
+        public Block(int size) : base()
+        {
+            Image image = Image.CreateRectangle(size);
+            AddGraphic(image);
+            image.CenterOrigin();
+        }
+
+        public void Spawn(Vector2 position, Color color)
+        {
+            Enable();
+            Position = position;
+            SetColor(color);
+        }
+
+        public void SetColor(Color color)
+        {
+            Graphic.Color = color;
+        }
+    }
+}
---
+++ WBGame/Misc/Bunch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WBGame.Pooling;
+
+namespace WBGame.Misc
+{
+    class Bunch : Poolable
+    {
+        Block[] blocks;
+
+        public Bunch(int size)
+        {
+            blocks = new Block[size];
+        }
+
+        public void SetBlock(Block block, int i)
+        {
+            blocks[i] = block;
+        }
+    }
+}
