c2b4f08bff1475b4cd452978fd40bc47ae8b0663
anvemaha <anvemaha@gmail.com>
2020-06-28 23:34:14 +0300

    Improve pooling

--- WBGame/Other/Pooler.cs
+++ WBGame/Other/Pooler.cs
@@ -4,12 +4,28 @@ using System.Collections;
 
 namespace WBGame.Other
 {
+    /// @author anvemaha
+    /// @version 28.6.2020
+    /// <summary>
+    /// Entity pooler. Production ready.
+    /// </summary>
+    /// <typeparam name="T">Poolable entity type</typeparam>
     public class Pooler<T> : IEnumerable where T : Poolable
     {
         private readonly T[] pool;
+        private readonly int lastIndex;
+        private int enablingIndex = 0;
 
+
+        /// <summary>
+        /// Initializes pool and poolable entities.
+        /// </summary>
+        /// <param name="scene">scene to add the objects to</param>
+        /// <param name="capacity">pool size</param>
+        /// <param name="entitySize">pooled entity size</param>
         public Pooler(Scene scene, int capacity, int entitySize)
         {
+            lastIndex = capacity - 1;
             pool = new T[capacity];
             for (int i = 0; i < capacity; i++)
             {
@@ -21,24 +37,63 @@ namespace WBGame.Other
         }
 
 
+        /// <summary>
+        /// Enables a disabled entity from the pool.
+        /// </summary>
+        /// <returns>Enabled entity</returns>
         public T Enable()
         {
-            foreach (T poolable in pool)
-                if (!poolable.Enabled)
+            if (enablingIndex == lastIndex && pool[enablingIndex].Enabled)
+            {
+                Defrag(); // Moves enablingIndex
+                if (enablingIndex == lastIndex)
                 {
-                    poolable.Enabled = true;
-                    return poolable;
+                    Console.WriteLine("[EMPTY] " + this);
+                    return null;
                 }
-            return null;
+            }
+            int current = enablingIndex;
+            pool[current].Enabled = true;
+            if (enablingIndex != lastIndex)
+                enablingIndex++;
+            return pool[current];
         }
 
 
-        public void Disable(T entity)
+        /// <summary>
+        /// Defragments the pool. This is way cheaper than looping through the entire pool every time something needs to be enabled.
+        /// </summary>
+        private void Defrag()
         {
-            entity.Enabled = false;
+            int i = 0;
+            while (i < enablingIndex)
+            {
+                if (pool[i].Enabled)
+                    i++;
+                else
+                {
+                    for (int j = enablingIndex; j > i; j--)
+                    {
+                        if (pool[j].Enabled)
+                        {
+                            T tmp = pool[j];
+                            pool[j] = pool[i];
+                            pool[i] = tmp;
+                            i++;
+                            break;
+                        }
+                        else
+                            enablingIndex--;
+                    }
+                }
+            }
         }
 
 
+        /// <summary>
+        /// So we can foreach through the pool. Once the project uses the new collision system and we don't need to debug pooling this can be removed.
+        /// </summary>
+        /// <returns>Pool enumerator</returns>
         public IEnumerator GetEnumerator()
         {
             return pool.GetEnumerator();
